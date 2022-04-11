da059f2d011acf4d2fe4d630b6e1a32e6be38e40
leonardocarloni <leonardocarloni@facef.br>
2022-04-11 16:23:08 -0300

    feat: add cache

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -3,7 +3,7 @@ import { RouteProps } from "react-router-dom";
 import Sobre from "../containers/sobre";
 import Combustivel from "../containers/combustivel";
 import StarWarsDetails from "../containers/star-wars-details";
-
+import Cache from "../containers/cache";
 
 const publicUrl = process.env.PUBLIC_URL;
 
@@ -17,4 +17,6 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
   { path: `${publicUrl}/star-wars/:id`, name: 'Star Wars', component: StarWarsDetails, exact: true },
+  { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
+
 ];
