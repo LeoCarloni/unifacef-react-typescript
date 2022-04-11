b36829d38ea61115fc8c45c99774921b7156bc7a
leonardocarloni <leonardocarloni@facef.br>
2022-04-11 16:52:54 -0300

    fix: route star-wars

--- src/routes/endpoints.ts
+++ src/routes/endpoints.ts
@@ -3,6 +3,7 @@ import { RouteProps } from "react-router-dom";
 import Sobre from "../containers/sobre";
 import Combustivel from "../containers/combustivel";
 import StarWarsDetails from "../containers/star-wars-details";
+import StarWars from "../containers/star-wars";
 import Cache from "../containers/cache";
 import Eleicoes from "../containers/eleicoes";
 import Register from '../containers/register';
@@ -20,7 +21,8 @@ export const endpoints: EndPointsProps[] = [
   { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
   { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
   { path: `${publicUrl}/sobre`, name: 'Sobre', component: Sobre, exact: true },
-  { path: `${publicUrl}/star-wars/:id`, name: 'Star Wars', component: StarWarsDetails, exact: true },
+  { path: `${publicUrl}/star-wars/`, name: 'Star Wars', component: StarWars, exact: true },
+  { path: `${publicUrl}/star-wars/:id`, name: 'Star Wars by ID', component: StarWarsDetails, exact: true },
   { path: `${publicUrl}/cache`, name: 'Cache', component: Cache, exact: true },
   { path: `${publicUrl}/eleicoes`, name: 'Eleições', component: Eleicoes, exact: true },
   { path: `${publicUrl}/register`, name: 'Register', component: Register, exact: true },
